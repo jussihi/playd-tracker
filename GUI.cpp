bef5207eaebe4e6923f27d09b2042cb3e8758ff6
Jussi Hietanen <jussi.hietanen@aalto.fi>
2018-09-11 13:11:20 +0300

    More work done for the GUI. Now resizes correctly

--- GUI.cpp
+++ GUI.cpp
@@ -3,6 +3,10 @@
 
 GUI::GUI()
 {
+  /* Initialize dimensions */
+  mOuterBoxPadding = 1;
+  mFirstTrack = 1;
+
   /* Initialize starting row to 0 */
   mCurrentRow = 0;
 
@@ -16,12 +20,17 @@ GUI::GUI()
   getmaxyx(stdscr, mScreenHeight, mScreenWidth);
 
   /* Create top window and Initialize (refresh) it */
-  mTopWindow = newwin(7, 72, 1, 1);
+  mTopWindow = newwin(7, 72, mOuterBoxPadding, mOuterBoxPadding);
   RefreshTopWindow();
   
   /* Create row window and initialize (refresh) it */
-  mRowWindow = newwin(5, 4, 9, 1);
+  mRowWindow = newwin(5, 4, 9, mOuterBoxPadding);
   RefreshRowWindow();
+
+  /* Create TrackNumberWindow and initialize (refresh) it */
+  int x = getmaxx(stdscr);
+  mTrackNumberWindow = newwin(1, x - 6, 8, 6);
+  RefreshTrackNumberWindow();
 
   /* Initialize windows with minimum of 4 instrument windows */
   for(int i = 0; i < 4; i++) mTrackWindows.push_back(nullptr);
@@ -81,19 +90,38 @@ bool GUI::CheckScreenSizeChanged()
     noecho();
     //clear();
     refresh();
+    RefreshDimensions();
     RefreshTopWindow();
     RefreshRowWindow();
+    RefreshTrackNumberWindow();
     changed = true;
   }
   return changed;
 }
 
+void GUI::RefreshDimensions()
+{
+  int x = getmaxx(stdscr);
+  mTracksToShow = (x-6) / 17;
+}
+
 
 void GUI::RefreshTopWindow()
 {
+  int y, x;
+  getmaxyx(stdscr, y, x);
   wclear(mTopWindow);
   delwin(mTopWindow);
-  mTopWindow = newwin(7, 72, 1, 1);
+
+  /* If the screen size is bigger than 74 columns, use max size */
+  if(x > 74)
+  {
+    mTopWindow = newwin(7, x-2, mOuterBoxPadding, mOuterBoxPadding);
+  }
+  else
+  {
+    mTopWindow = newwin(7, 72, mOuterBoxPadding, mOuterBoxPadding);
+  }
   box(mTopWindow, 0, 0);
 
   refresh();
@@ -101,7 +129,6 @@ void GUI::RefreshTopWindow()
   mvwprintw(mTopWindow, 0, 6, "playd trckr V0.01"); 
   mvwprintw(mTopWindow, 0, 32, "Copyright 2018 Jussi Hietanen");
 
-  int x,y;
   getmaxyx(mTopWindow, y, x);
 
   mvwprintw(mTopWindow, 1, 6, "x: %d, y: %d", x, y);
@@ -113,11 +140,60 @@ void GUI::RefreshRowWindow()
 {
   wclear(mRowWindow);
   delwin(mRowWindow);
-  mRowWindow = newwin(5, 4, 9, 1);
+  mRowWindow = newwin(5, 4, 9, mOuterBoxPadding);
   int y, x;
   getmaxyx(stdscr, y, x);
-  wresize(mRowWindow, y - 9, 4);
+
+  /* set basic boundaries for row count, set height here */
+  if( y-9 > 64)
+  {
+    wresize(mRowWindow, 64, 4);
+  }
+  else
+  {
+    wresize(mRowWindow, y - 9, 4);
+  }
   box(mRowWindow, 0, 0);
   refresh();
   wrefresh(mRowWindow);
+}
+
+void GUI::RefreshTrackNumberWindow()
+{
+  int x = getmaxx(stdscr);
+  wclear(mTrackNumberWindow);
+  delwin(mTrackNumberWindow);
+  mTrackNumberWindow = newwin(1, x - 6, 8, 6);
+
+  
+  for(int i = 0; i < mTracksToShow; i++)
+  {
+    mvwprintw(mTrackNumberWindow, 0, i*17, "     %02d", mFirstTrack + i);
+  }
+  refresh();
+  wrefresh(mTrackNumberWindow);
+}
+
+void GUI::RefreshTrackWindows()
+{
+  /* Check if the number of tracks to be shown has changed */
+  if(mTrackWindows.size != mTracksToShow)
+  {
+    if(mTrackWindows.size > mTracksToShow)
+    {
+
+    }
+    else
+    {
+
+    }
+  }
+
+  /* Here load data from tracker side to GUI */
+
+  /* Print the new windows */
+  for(int i = 0; i < )
+  {
+    
+  }
 }
--- GUI.hpp
+++ GUI.hpp
@@ -8,15 +8,27 @@ public:
   ~GUI();
   void GetInput();
   bool CheckScreenSizeChanged();
+  void RefreshDimensions();
   void RefreshTopWindow();
   void RefreshRowWindow();
+  void RefreshTrackNumberWindow();
+  void RefreshTrackWindows();
 private:
   int mScreenWidth;
   int mScreenHeight;
   int mCurrentRow;
+  int mFirstTrack;
+
+  /* dimension-specific variables */
+  int mOuterBoxPadding;
+  int mTracksToShow;
+  int mRowsToShow;
+
 
   WINDOW* mTopWindow;
   WINDOW* mRowWindow;
   WINDOW* mTrackNumberWindow;
   std::deque<WINDOW *> mTrackWindows;
+
+  
 };
